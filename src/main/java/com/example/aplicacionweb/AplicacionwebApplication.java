2c423d4e46101fd110471ead1e88a75df8c36f66
Snayder07 <snayder071207@email.com>
2026-07-17 19:56:28 -0500

    Primer commit del proyecto AplicacionWEB

---
+++ src/main/java/com/example/aplicacionweb/AplicacionwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.aplicacionweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aplicacionweb/AplicacionwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aplicacionweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
